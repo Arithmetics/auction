aedd68e8cb30ac6cd81016e7859f8576748fdc59
Arithmetics <brock.m.tillotson@gmail.com>
2018-02-14 11:01:52 -0800

    fixed fixtures issue, tests should be running again

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,6 +3,11 @@ class Bid < ApplicationRecord
   belongs_to :draft
   belongs_to :player
 
+  validates :amount, presence: true
+  validates :user_id, presence: true
+  validates :draft_id, presence: true
+  validates :player_id, presence: true
+
 
 
 end
--- test/models/bid_test.rb
+++ test/models/bid_test.rb
@@ -4,4 +4,27 @@ class BidTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @draft2018 = drafts(:six)
+    @player1 = players(:one)
+    @player2 = players(:two)
+    @user1 = users(:one)
+  end
+
+  test "valid bid has user" do
+    @new_bid = Bid.new(
+      draft: @draft2018,
+      player: @player1,
+      user: @user1,
+      winning: false
+    )
+
+    assert_not @new_bid.valid?
+  end
+
+
+
+
+
 end
