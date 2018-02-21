272a9d09191fd6a78dd2bcf8e40337c33681e3e8
Arithmetics <brock.m.tillotson@gmail.com>
2018-02-21 12:41:58 -0800

    nav looks great!

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,3 +1,11 @@
 $(document).on('turbolinks:load', function(){
-
+  const x = document.getElementById("myTopnav");
+  x.addEventListener('click', function(){
+    console.log('fusdf')
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+  })
 })
